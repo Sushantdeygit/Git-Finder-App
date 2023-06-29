3d2b45c0199f3274181dc79f4405b829d96bac89
Sushant <sushantdey089@gmail.com>
2023-06-30 02:45:04 +0530

    final

--- src/data/api.jsx
+++ src/data/api.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const accessToken="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";
+const accessToken="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";
 
 
 
